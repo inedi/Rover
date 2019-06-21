0360b907bf487c3612ad2c8dacd822277d4e32a9
INEDI <inedi@yandex.ru>
2019-06-21 15:15:48 +0300

    start

---
+++ Moon Rover/Assets/Views/MainView.xaml.cs
@@ -0,0 +1,25 @@
+﻿#if UNITY_5_3_OR_NEWER
+#define NOESIS
+using Noesis;
+#else
+using System;
+using System.Windows.Controls;
+#endif
+
+namespace Rover.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+#if NOESIS
+        private void InitializeComponent()
+        {
+            GUI.LoadComponent(this, "Assets/Views/Pages/StartPage.xaml");
+        }
+#endif
+    }
+}
