72501d1e81027feaa7d963afdb87c37b03ff357e
INEDI <inedi@yandex.ru>
2019-07-20 21:19:24 +0300

    Radar

---
+++ Rover/Assets/Scripts/PostprosessCameraShaderEffect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class PostprosessCameraShaderEffect : MonoBehaviour
+{
+    [SerializeField]
+    private Material material;
+
+    private Camera camera;
+
+    void Awake()
+    {
+        camera = GetComponent<Camera>();
+        camera.depthTextureMode = camera.depthTextureMode | DepthTextureMode.DepthNormals;
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, material);
+    }
+}
